53f30a1b3acdb4d88a23dab214b7ec067f2d5fb1
João Vitor Vale <joaovitorvalec@gmail.com>
2025-12-15 16:25:26 -0300

    chore: migrar base para expo

--- App.tsx
+++ App.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
-import LoginScreen from './src/screens/loginScreen';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-const App = () => {
-  return <LoginScreen />;
-};
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
