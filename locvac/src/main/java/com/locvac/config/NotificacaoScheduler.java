2550842e402d4728f75f84355f640fa995eead00
João Vitor Vale <joaovitorvalec@gmail.com>
2026-05-02 12:02:59 -0300

    test

--- locvac/src/main/java/com/locvac/config/NotificacaoScheduler.java
+++ locvac/src/main/java/com/locvac/config/NotificacaoScheduler.java
@@ -58,7 +58,7 @@ public class NotificacaoScheduler {
     }
 
     @Scheduled(cron = "0 0 7 * * *", zone = "America/Sao_Paulo")
-    @Scheduled(cron = "0 0 11 * * *", zone = "America/Sao_Paulo")
+    @Scheduled(cron = "0 30 12 * * *", zone = "America/Sao_Paulo")
     @Transactional
     public void executar() {
         log.info("Iniciando varredura diária de notificações");
