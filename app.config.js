98291a8ac7c8d4dd05ac35eafe4607c1a54add69
João Vitor Vale <joaovitorvalec@gmail.com>
2026-05-25 00:41:59 -0300

    refactor: versão

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ module.exports = {
   expo: {
     name: IS_DEV ? "LocVac (Dev)" : "LocVac",
     slug: "LocVac",
-    version: "1.0.2",
+    version: "1.0.3",
     orientation: "portrait",
     icon: "./assets/images/iconLocVac.png",
     userInterfaceStyle: "light",
